26d83bbbb112c8bb965cdc41640e3f7a1fd4beac
rl2939 <60378449+rl2939@users.noreply.github.com>
2025-02-24 16:08:18 -0500

    added admin2 for testing purposes.

---
+++ static/js/admin2.js
@@ -0,0 +1,51 @@
+const listProfiles = async () => {
+	const endpoint = "http://localhost:8080/admin/profiles/1";
+	try {
+    	const response = await fetch(endpoint);
+	    if (!response.ok) {
+	      throw new Error(`Response status: ${response.status}`);
+	    }
+
+	    const json = await response.json();
+	    console.log(json);
+      return json
+	 } catch (error) {
+	    console.error(error.message);
+  }
+}
+
+const addTableRows = (rows) => {
+	table = document.getElementById("orgMembers");
+
+  rows.forEach(row => {
+    var newElement = `
+      <tr class="dbTableRow">
+                  <td>
+                      <label class="container">
+                          <input type="checkbox" >
+                          <span class="checkmark"></span>
+                      </label>
+                  </td>
+                  <td>${row['profile']['first_name']}</td>
+                  <td>${row['profile']['last_name']}</td>
+                  <td>Undetermined</td>
+                  <td>Undetermined</td>
+                  <td>${row['profile']['email']}</td>
+                  <td>Undetermined</td>
+                  <td>Undetermined</td>
+                  <td>Undetermined</td>
+                  <td>Undetermined</td>
+                  <td>Undetermined</td>
+                  <td class="dbTablePH"></td>
+                  <td><img src="../static/images/options.png" width="16" /></td>
+              </tr>
+      `
+    table.insertAdjacentHTML( 'beforeend', newElement )
+  });
+    
+}
+
+
+listProfiles().then(
+  addTableRows
+)

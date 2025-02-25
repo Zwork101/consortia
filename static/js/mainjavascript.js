b7be3a9f2218697490af80ce74a3a1b925c64b7a
BlankaPeller <blanka.peller@outlook.com>
2025-02-25 09:51:32 -0500

    Adds WIC student view with interactibility

--- static/js/mainjavascript.js
+++ static/js/mainjavascript.js
@@ -56,3 +56,73 @@ window.onclick = function (event) {
         }
     }
 }
+
+$(document).ready(function () {
+    let currentDate = new Date();
+
+    function renderCalendar(date) {
+        let month = date.toLocaleString('default', { month: 'long' });
+        let year = date.getFullYear();
+        $('#month-title').text(`${month} ${year}`);
+
+        let firstDay = new Date(year, date.getMonth(), 1).getDay();
+        let lastDate = new Date(year, date.getMonth() + 1, 0).getDate();
+
+        let calendarHTML = "";
+        let weekdays = ["S", "M", "T", "W", "T", "F", "S"];
+        weekdays.forEach(day => {
+            calendarHTML += `<div class="weekday">${day}</div>`;
+        });
+        for (let i = 0; i < firstDay; i++) {
+            calendarHTML += '<div class="day empty"></div>';
+        }
+        for (let i = 1; i <= lastDate; i++) {
+            calendarHTML += `<div class="day" data-date="${year}-${date.getMonth() + 1}-${i}">${i}</div>`;
+        }
+        $('.calendar-grid').html(calendarHTML);
+
+        // Highlight today's date by default
+        let today = new Date();
+        if (today.getMonth() === date.getMonth() && today.getFullYear() === date.getFullYear()) {
+            $('.day').each(function () {
+                if ($(this).text() == today.getDate()) {
+                    $(this).addClass('selected');
+                }
+            });
+        }
+
+        // Add click event to select a date
+        $('.day').click(function () {
+            $('.day').removeClass('selected');
+            $(this).addClass('selected');
+            let selectedDate = $(this).data('date');
+            console.log(selectedDate); // Log the selected date
+        });
+    }
+
+    renderCalendar(currentDate);
+
+    $('#prev').click(function () {
+        currentDate.setMonth(currentDate.getMonth() - 1);
+        renderCalendar(currentDate);
+    });
+
+    $('#next').click(function () {
+        currentDate.setMonth(currentDate.getMonth() + 1);
+        renderCalendar(currentDate);
+    });
+});
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}

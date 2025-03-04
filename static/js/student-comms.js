f5c58b888e68103f0b8cf57b411d003bef52ac69
rl2939 <60378449+rl2939@users.noreply.github.com>
2025-03-04 17:34:41 -0500

    Add student-comms.js updates

--- static/js/student-comms.js
+++ static/js/student-comms.js
@@ -1,3 +1,10 @@
+let points = 0
+let maxPoints = 20
+let mentorshipMeetingCount = 0
+let voluenteeringCount = 0
+let attendenceCount = 0
+let miscCount = 0
+
 const getProfile = async () => {
 	const endpoint = "http://localhost:8080/profile/1?org_id=1";
 	try {
@@ -20,15 +27,22 @@ const getStudentPoints = (studentData) => {
   console.log(studentData)
   console.log(studentData.profile.attendance)
 
-  let attendance = studentData.profile.attendance
-  let mentorshipMeetingCount = 0
-  let voluenteeringCount = 0
-  let attendenceCount = 0
-  let miscCount = 0
+  if (studentData.profile.membership == "active"){
+    points += 3
+    console.log(points)
+  } 
 
+  let attendance = studentData.profile.attendance
   attendance.forEach(attendanceDay =>{
-    console.log(attendanceDay)
+    //console.log(attendanceDay)
+    console.log(attendanceDay.meeting_type)
+    if (attendanceDay.meeting_type == "GENERAL"){
+      points += 1
+      console.log(points)
+    }
+    document.getElementById("mentor-bar").style.width = `${(points/maxPoints)*100}%`
   })
+
     
 }
 

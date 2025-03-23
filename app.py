acd5b04a31d65dbaae2447de5e4fc0aacff5e999
Nathan Zilora <zwork101@gmail.com>
2025-03-23 15:06:46 -0400

    Logging Information

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, Blueprint, request
 
 def create_app(config_file: Config = DevelopmentConfig) -> Flask:
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, filename="development.log", filemode="a")
     # logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
     
     app = Flask(__name__, static_folder="static/", template_folder="templates/")

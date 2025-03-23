27399717b04b8bab9b50ee5066474af0ea2e3442
BlankaPeller <blanka.peller@outlook.com>
2025-03-23 01:34:20 -0400

    converts wics and coms profiles to j2

--- backend/routes/testing.py
+++ backend/routes/testing.py
@@ -34,7 +34,7 @@ def return_db_wic():
 
 @test.route("/testt")
 def return_testt():
-    return render_template("comsemail.html.j2")
+    return render_template("wics-profile.html.j2")
 
 @test.route("/com-email")
 def return_com_email():

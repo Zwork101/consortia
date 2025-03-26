8c51716e8254a8775006648fbe6a56ce0f11c103
BlankaPeller <blanka.peller@outlook.com>
2025-03-26 13:00:14 -0400

    updates student profile page

--- backend/routes/testing.py
+++ backend/routes/testing.py
@@ -40,6 +40,8 @@ def return_wic():
 def return_coms():
     return render_template("coms-profile.html.j2")
 
+
+
 @test.route("/com-email")
 def return_com_email():
     return render_template("comsemail.html.j2", member_name="John Doe", points="5", events=[{

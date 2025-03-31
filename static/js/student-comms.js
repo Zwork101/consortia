9c1781a3cd5bdda6a02799324ff0ff01989b4173
rl2939 <60378449+rl2939@users.noreply.github.com>
2025-03-31 00:22:29 -0400

    Comment out unused code.

--- static/js/student-comms.js
+++ static/js/student-comms.js
@@ -140,17 +140,15 @@ const getStudentPoints = (endpointData) => {
 
   historyBuilder(builderMode.COMS, studentData, allMeetingData);
 }
+// const loadSemesters = () => {
+//   const container = document.getElementById("semesters");
 
-
-const loadSemesters = () => {
-  const container = document.getElementById("semesters");
-
-  semesters.forEach(sem => {
-      const semElement = document.createElement("div");
-      semElement.innerHTML = 
-      `<h3>${sem.semester_coms} ${sem.year_coms}</h3>` +
-      `<p>${sem.organizer_coms}, ${sem.meeting_type_coms}: ${sem.description_coms}, ${sem.point_value_coms}</p>`;
-      container.appendChild(semElement);
-    }
-  );
-};
+//   semesters.forEach(sem => {
+//       const semElement = document.createElement("div");
+//       semElement.innerHTML = 
+//       `<h3>${sem.semester_coms} ${sem.year_coms}</h3>` +
+//       `<p>${sem.organizer_coms}, ${sem.meeting_type_coms}: ${sem.description_coms}, ${sem.point_value_coms}</p>`;
+//       container.appendChild(semElement);
+//     }
+//   );
+// };

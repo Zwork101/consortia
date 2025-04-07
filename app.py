10a7aff38b77991cd666a260850bdb4795817c93
Nathan Zilora <zwork101@gmail.com>
2025-04-07 15:52:02 -0400

    Dynamically load Org IDs

--- app.py
+++ app.py
@@ -44,23 +44,16 @@ def create_app(config_file: Config = DevelopmentConfig) -> Flask:
 
         defacto_admin = db.session.query(Administrator.id).join(Profile).where(Profile.rit_id == app.config['DEFACTO_ADMIN']['rit_id']).first()
         if defacto_admin is None:
-            WiC = Organizer(
-                name = "Women in Computing",
-                email = "wic@rit.edu"
-            )
-            
-            COMS = Organizer(
-                name = "Computing Organization for Multicultural Students",
-                email = "coms@rit.edu"
-            )
-
             profile = db.session.query(Profile.rit_id).where(Profile.rit_id == app.config['DEFACTO_ADMIN']['rit_id']).first()
             if profile is None:
                 profile = Profile(**app.config['DEFACTO_ADMIN'])
-            commit(profile, WiC, COMS)
+            commit(profile)
             make_admin(profile.profile_id, Organizations.COMS, RoleType.ADMIN)
             make_admin(profile.profile_id, Organizations.WIC, RoleType.ADMIN)
             commit()
+
+        Organizations.COMS = db.session.query(Organizer.organization_id).where(Organizer.name == "Computing Organization for Multicultural Students").first()[0]
+        Organizations.WIC = db.session.query(Organizer.organization_id).where(Organizer.name == "Women in Computing").first()[0]
 
     return app
 

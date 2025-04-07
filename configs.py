1cca5eeadb863f35c6304130c85649997665a4fe
Nathan Zilora <zwork101@gmail.com>
2025-04-07 13:06:40 -0400

    Prepare production environment

--- configs.py
+++ configs.py
@@ -64,4 +64,4 @@ class LocalConfig(Config):
 class ProductionConfig(Config):
     USE_X_SENDFILE = True
     SERVER_NAME = "consortia.gccis.rit.edu"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
+    SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://consortia:{os.environ["DB_PASSWORD"]}@localhost:3306/consortia"

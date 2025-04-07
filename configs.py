1149065b32b673f96eaee71fdf5cb43833276a03
root <root@consortia.gccis.rit.edu>
2025-04-07 13:24:48 -0400

    Fix issues

--- configs.py
+++ configs.py
@@ -64,4 +64,4 @@ class LocalConfig(Config):
 class ProductionConfig(Config):
     USE_X_SENDFILE = True
     SERVER_NAME = "consortia.gccis.rit.edu"
-    SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://consortia:{os.environ["DB_PASSWORD"]}@localhost:3306/consortia"
+    SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://consortia:{os.environ['DB_PASSWORD']}@localhost:3306/consortia"

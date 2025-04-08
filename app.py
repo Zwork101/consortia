9cae47122427a253010d0d117b74dae5492794f5
Nathan Zilora <zwork101@gmail.com>
2025-04-08 17:30:58 -0400

    Don't take production action in dev

--- app.py
+++ app.py
@@ -39,22 +39,24 @@ def create_app(config_file: Config = DevelopmentConfig) -> Flask:
 
     app.jinja_env.add_extension("jinja2.ext.loopcontrols")
 
-    with app.app_context():
-        db.create_all()
+    if config_file is not DevelopmentConfig:
 
-        Organizations.COMS = db.session.query(Organizer.organization_id).where(Organizer.name == "Computing Organization for Multicultural Students").first()[0]
-        Organizations.WIC = db.session.query(Organizer.organization_id).where(Organizer.name == "Women in Computing").first()[0]
+        with app.app_context():
+            db.create_all()
 
-        defacto_admin = db.session.query(Administrator.id).join(Profile).where(Profile.rit_id == app.config['DEFACTO_ADMIN']['rit_id']).first()
-        if defacto_admin is None:
-            profile = db.session.query(Profile.rit_id).where(Profile.rit_id == app.config['DEFACTO_ADMIN']['rit_id']).first()
-            if profile is None:
-                profile = Profile(**app.config['DEFACTO_ADMIN'])
-            commit(profile)
-            print("Making admin")
-            make_admin(profile.profile_id, Organizations.COMS, RoleType.ADMIN)
-            make_admin(profile.profile_id, Organizations.WIC, RoleType.ADMIN)
-            commit()
+            Organizations.COMS = db.session.query(Organizer.organization_id).where(Organizer.name == "Computing Organization for Multicultural Students").first()[0]
+            Organizations.WIC = db.session.query(Organizer.organization_id).where(Organizer.name == "Women in Computing").first()[0]
+
+            defacto_admin = db.session.query(Administrator.id).join(Profile).where(Profile.rit_id == app.config['DEFACTO_ADMIN']['rit_id']).first()
+            if defacto_admin is None:
+                profile = db.session.query(Profile.rit_id).where(Profile.rit_id == app.config['DEFACTO_ADMIN']['rit_id']).first()
+                if profile is None:
+                    profile = Profile(**app.config['DEFACTO_ADMIN'])
+                commit(profile)
+                print("Making admin")
+                make_admin(profile.profile_id, Organizations.COMS, RoleType.ADMIN)
+                make_admin(profile.profile_id, Organizations.WIC, RoleType.ADMIN)
+                commit()
 
     return app
 

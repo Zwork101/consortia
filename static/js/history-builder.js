effc3e038e1e44f3ef29cb1957132046836470b1
rl2939 <60378449+rl2939@users.noreply.github.com>
2025-04-09 04:53:05 -0400

    Increased scope of some variables.

--- static/js/history-builder.js
+++ static/js/history-builder.js
@@ -104,17 +104,17 @@ function historyBuilder(mode, userObject, allMeetingsObject, settingsAndConfigOb
 
     let configData = settingsAndConfigObject.config;
     let sortedUserMeetings = meetingsSortedBySemester(userObject.profile.attendance, sortingOrder.Descending);
+    let sortedAllMeetings = meetingsSortedBySemester(allMeetingsObject.Meetings);
     let userAwards = convertAwardDatesToCustomTimestamp(userObject.profile.awards);
     sortedUserMeetings.forEach(userSemester => {
         semesterDateData = semesterTermAndYear(userSemester.semester);
         let isUserActive = activeUser.NonActive;
         let awardValues, awardImage = ``
+        let matchedSemester = sortedAllMeetings.find(semesterDate => semesterDate.semester === userSemester.semester);
         if (mode == builderMode.WIC){
             //console.log("WICMODE")
 
             // Find the semester in allMeetings Object
-            let sortedAllMeetings = meetingsSortedBySemester(allMeetingsObject.Meetings);
-            let matchedSemester = sortedAllMeetings.find(semesterDate => semesterDate.semester === userSemester.semester);
             let userMeetingDataForThisSemester = getMeetingData(userSemester.meetings);
             let allMeetingDataForThisSemester = getMeetingData(matchedSemester.meetings);
             awardValues = `
